cd5ddf7d3b5c7c7b17c7ea483d6efe4e71160cd7
thwls475 <thwls475@gmail.com>
2022-11-03 03:27:17 +0900

    Slide Toggle

--- js/main.js
+++ js/main.js
@@ -60,6 +60,7 @@ new Swiper( '.promotion .swiper-container', {
     loop: true,
     // autoplay: {
     //     delay: 5000
+    //},
     
     pagination: {
         el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
@@ -68,5 +69,20 @@ new Swiper( '.promotion .swiper-container', {
     navigation: {
         prevEl: '.promotion .swiper-prev',
         nextEl: '.promotion .swiper-next'
+    }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) {
+        //숨김 처리
+        promotionEl.classList.add('hide');
+    } else {
+        //보임 처리
+        promotionEl.classList.remove('hide')
     }
 });
