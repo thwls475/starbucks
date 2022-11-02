e1fc5cf7329884e72f5e418f85917d64dfa63021
thwls475 <thwls475@gmail.com>
2022-11-03 02:41:48 +0900

    Promotion Image Slide_1

--- js/main.js
+++ js/main.js
@@ -51,5 +51,22 @@ fadeEls.forEach(function (fadeEl, index) {
 new Swiper('.notice-line .swiper-container', {
     direction: 'vertical',
     autoplay: true,
-    loop: true
+    loop: true,
+});
+new Swiper( '.promotion .swiper-container', {
+    slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이 여백
+    centeredSlides: true, //1번 슬라이드가 가운데 보이기
+    loop: true,
+    // autoplay: {
+    //     delay: 5000
+    
+    pagination: {
+        el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
 });
